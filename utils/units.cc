29643d1c4092ef1895e9b54a3187657eb53abddb
Leandro Rabindranath Leon <leandro.r.leon@gmail.com>
2016-08-03 21:10:13 -0400

    Unidades dinámicas por nombres y símbolos

--- utils/units.cc
+++ utils/units.cc
@@ -5,3 +5,8 @@ UnitItemTable PhysicalQuantity::tbl;
 
 UnitItemTable Unit::tbl;
 DynSetTree<const Unit*> Unit::unit_tbl;
+
+UnitHashTbl __unit_name_name_tbl;
+UnitHashTbl __unit_name_symbol_tbl;
+UnitHashTbl __unit_symbol_name_tbl;
+UnitHashTbl __unit_symbol_symbol_tbl;
