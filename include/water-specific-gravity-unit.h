56b3cfe8f3c68ce2e24c15002b816480971c6ad5
Alberto Valderrama <albertoevalderrama@gmail.com>
2016-08-14 22:32:50 +0000

    Replace water-specific-gravity-unit.h, se remplaza por error en la palabra specific

--- include/water-specific-gravity-unit.h
+++ include/water-specific-gravity-unit.h
@@ -8,7 +8,7 @@
 // SOURCE: McCain W.D, Jr: The properties of Petroleum Fluid 2nd. ed Tulsa, OK PennWell 
 // SOURCE: Carlos Banzer S, Correlaciones Numericas P.V.T. Instituto de Investigaciones Petroleras Universidad del Zulia  
 
-Declare_Physical_Quantity(WaterSpecicGravity, "Sgw",
+Declare_Physical_Quantity(WaterSpecificGravity, "Sgw",
 		R"DESC(density of a brine (in this case formation water) in normal conditions based on total solid disolved, 
 		it is defined as the ratio of the density of the brine to the density pure water, both taken at 
 		the same pressure and temperature (usually atmopheric pressure to the seal level and 60 degF)
@@ -16,20 +16,20 @@ Declare_Physical_Quantity(WaterSpecicGravity, "Sgw",
 
 Declare_Unit(pwl_lb_ft3, "pwl_lb/ft3",
 	     R"DESC(unit used to measure desity of produce water (brine) in normal conditions 
-	     denoted as pound per cubic feet)DESC", WaterSpecicGravity, 62.366389027, 78);
+	     denoted as pound per cubic feet)DESC", WaterSpecificGravity, 62.366389027, 78);
 
 
 Declare_Unit(Sgw_Percent, "sgwPercent",
 	     R"DESC(Unit used to measure salinity of a brine
-	      in normal conditions in percentage of disolved solid)DESC", WaterSpecicGravity, 1, 1.2506);
+	      in normal conditions in percentage of disolved solid)DESC", WaterSpecificGravity, 1, 1.2506);
 
 Declare_Unit(Sgw_sg, "swsg",
 	     R"DESC(Specific gravity of a brine in normal conditions, it is the ratio of the density of a brine to the 
-	     density of a pure water;)DESC", WaterSpecicGravity, 1, 1.2506);
+	     density of a pure water;)DESC", WaterSpecificGravity, 1, 1.2506);
 
 Declare_Unit(Sgw_PPM, "swppm",
 	     R"DESC(Total solid disolved in a brine
-	      measure in Part Per Million PPM)DESC", WaterSpecicGravity, 0, 317642.11170139315);
+	      measure in Part Per Million PPM)DESC", WaterSpecificGravity, 0, 317642.11170139315);
 
 //To Water Specific Gravity in lb/ft3
 Declare_Conversion( Sgw_Percent, pwl_lb_ft3, v) { return  62.366389027 + 0.438603 * v +  0.00160074 * pow( v , 2) ;}
