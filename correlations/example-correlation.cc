a05bc8cd6fad1e49545f812c9aa8b722626bd146
Leandro Rabindranath Leon <leandro.r.leon@gmail.com>
2016-08-16 12:04:27 -0400

    Corregidos detalles de nombres en nuevas conversiones y avances en definción de correlaciones

--- correlations/example-correlation.cc
+++ correlations/example-correlation.cc
@@ -1,12 +1,12 @@
 
 # include <iostream>
 
-# include <correlation.H>
 # include <pvt-units.H>
+# include <pvt-correlations.H>
 
 int main()
 {
-  OilCorrelation c("Test", Celsius::get_instance(), 0, 200);
+  PbAlMarhounCorrelation c(Celsius::get_instance(), 0, 200);
   c.add_parameter("p1", "degF", 40, 90);
   c.add_parameter("p2", "psia", 4, 10);
 
