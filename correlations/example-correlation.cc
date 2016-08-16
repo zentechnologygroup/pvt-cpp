be71153169a96fd02b91eb9e8f9bf990e4c8bb91
Leandro Rabindranath Leon <leandro.r.leon@gmail.com>
2016-08-15 21:44:18 -0400

    Más correlacion

--- correlations/example-correlation.cc
+++ correlations/example-correlation.cc
@@ -7,6 +7,8 @@
 int main()
 {
   OilCorrelation c("Test", Celsius::get_instance(), 0, 200);
+  c.add_parameter("p1", "degF", 40, 90);
+  c.add_parameter("p2", "psia", 4, 10);
 
   cout << c << endl;
   
