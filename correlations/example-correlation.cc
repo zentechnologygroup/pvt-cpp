91c84eb9f772bc51f527cc325f410ca5f27b6777
Leandro Rabindranath Leon <leandro.r.leon@gmail.com>
2016-08-16 20:12:57 -0400

    Casi lista 1ra corr.
    Falta validación genérica de parámetros

--- correlations/example-correlation.cc
+++ correlations/example-correlation.cc
@@ -6,9 +6,9 @@
 
 int main()
 {
-  PbAlMarhounCorrelation c(Celsius::get_instance(), 0, 200);
-  c.add_parameter("p1", "degF", 40, 90);
-  c.add_parameter("p2", "psia", 4, 10);
+  PbAlMarhounCorrelation c;
+  // c.add_parameter("p1", "degF", 40, 90);
+  // c.add_parameter("p2", "psia", 4, 10);
 
   cout << c << endl;
   
