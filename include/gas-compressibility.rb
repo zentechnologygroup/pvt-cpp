750eae391df45f2c066f6d7f386434dd72d7869b
Leandro Rabindranath Leon <leandro.r.leon@gmail.com>
2016-09-20 13:36:58 -0400

    Más comprensiilidad

--- include/gas-compressibility.rb
+++ include/gas-compressibility.rb
@@ -32,3 +32,12 @@ add_parameter("p", "psia")
 add_parameter("tsc", "Rankine") # TODO falta cotas
 add_parameter("psc", "psia")
 end_correlation()
+
+################################################################
+
+begin_correlation("ZFactorGopal", "GasCompressibility", "ZFactor")
+add_parameter("t", "Rankine")
+add_parameter("p", "psia")
+add_parameter("tsc", "Rankine") # TODO falta cotas
+add_parameter("psc", "psia")
+end_correlation()
