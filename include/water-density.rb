be92c772f120ba0528a8c2068f2e5d6cdce02c44
Leandro Rabindranath Leon <leandro.r.leon@gmail.com>
2016-09-26 18:03:21 -0400

    Empezadas correlaciones de agua

---
+++ include/water-density.rb
@@ -0,0 +1,23 @@
+
+begin_correlation("PwSpiveyMN", "WaterDensity", "Gr_cm3")
+add_parameter("t", "Celsius",
+              "Quantity<Fahrenheit>(32)", "Quantity<Fahrenheit>(530)")
+add_parameter("pr", "Pascal", "Quantity<psia>(14.5)", "Quantity<psia>(29000)")
+add_parameter("s", "Sgw_PPM", 0, 26)
+end_correlation()
+
+################################################################
+
+begin_correlation("PwSpiveyMNGasFree", "WaterDensity", "Gr_cm3")
+add_parameter("t", "Celsius",
+              "Quantity<Fahrenheit>(32)", "Quantity<Fahrenheit>(530)")
+add_parameter("pr", "Pascal", "Quantity<psia>(14.5)", "Quantity<psia>(29000)")
+add_parameter("s", "Sgw_PPM", 0, 26)
+end_correlation()
+
+################################################################
+
+begin_correlation("PwMcCain", "WaterDensity", "Lb_ft3")
+add_parameter("s", "Sgw_PPM", 0, 26)
+add_parameter("bw", "RB_STB")
+end_correlation()
