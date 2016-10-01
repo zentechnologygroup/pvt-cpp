f9020a41ba4086f5f8dc6ec608a22f326324c4b1
Leandro Rabindranath Leon <leandro.r.leon@gmail.com>
2016-10-01 17:03:25 -0400

    olvido

--- pvt-calc/mainwindow.cpp
+++ pvt-calc/mainwindow.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+# include <correlation.H>
+
 MainWindow::MainWindow(QWidget *parent) :
   QMainWindow(parent),
   ui(new Ui::MainWindow)
 {
   ui->setupUi(this);
+
+  for (auto it = Corr
 }
 
 MainWindow::~MainWindow()
