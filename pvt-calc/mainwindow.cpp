8ad18ce8105cfecfeb60167a4443d48386d0d673
Leandro Rabindranath Leon <leandro.r.leon@gmail.com>
2016-10-01 22:30:12 -0400

    Avanzando

--- pvt-calc/mainwindow.cpp
+++ pvt-calc/mainwindow.cpp
@@ -8,8 +8,51 @@
 void MainWindow::set_substype_combo(const string &type_name)
 {
   subtypes = Correlation::subtype_list(type_name);
+
+  ui->corr_subtype_combo->clear();
   for (auto it = subtypes.get_it(); it.has_curr(); it.next())
     ui->corr_subtype_combo->addItem(QString::fromStdString(it.get_curr()));
+
+  auto first = subtypes.get_first();
+  set_corr_combo(first);
+}
+
+void MainWindow::set_corr_combo(const string &subtype_name)
+{
+  correlations = Correlation::list(subtype_name);
+
+  ui->corr_combo->clear();
+  for (auto it = correlations.get_it(); it.has_curr(); it.next())
+    {
+      auto ptr = it.get_curr();
+      ui->corr_combo->addItem(QString::fromStdString(ptr->name));
+    }
+
+  auto correlation = correlations.get_first();
+  auto text = correlation->full_desc(40);
+  set_tech_note(text);
+  build_corr_entries(correlation->name);
+}
+
+void MainWindow::set_tech_note(const string &note)
+{
+  ui->tech_note->setText(QString::fromStdString(note));
+}
+
+void MainWindow::build_corr_entries(const string &corr_name)
+{
+  auto frame = ui->parameters_area;
+
+  pars_vals.for_each([] (auto ptr) { delete ptr; });
+  pars_vals.empty();
+  auto correlation = Correlation::search_by_name(corr_name);
+  auto pars = correlation->get_preconditions();
+  for (auto it = pars.get_it(); it.has_curr(); it.next())
+    {
+      const auto & par = it.get_curr();
+      QLineEdit * edt = new QLineEdit(this);
+      pars_vals.append(edt);
+    }
 }
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -18,19 +61,11 @@ MainWindow::MainWindow(QWidget *parent) :
 {
   ui->setupUi(this);
 
-  auto types = Correlation::type_list();
+  types = Correlation::type_list();
   for (auto it = types.get_it(); it.has_curr(); it.next())
     ui->corr_type_combo->addItem(QString::fromStdString(it.get_curr()));
 
   set_substype_combo(types.get_first());
-
-  auto correlations = Correlation::list(subtypes.get_first());
-  auto correlation = correlations.get_first();
-
-  for (auto it = correlations.get_it(); it.has_curr(); it.next())
-    ui->corr_combo->addItem(QString::fromStdString(it.get_curr()->name));
-
-  ui->tech_note->setText(QString::fromStdString(correlation->full_desc(50)));
 }
 
 MainWindow::~MainWindow()
@@ -38,7 +73,22 @@ MainWindow::~MainWindow()
   delete ui;
 }
 
-void MainWindow::on_corr_type_combo_activated(int index)
-{
 
+void MainWindow::on_corr_type_combo_activated(const QString &arg1)
+{
+  auto type_name = arg1.toStdString();
+  set_substype_combo(type_name);
+}
+
+void MainWindow::on_corr_subtype_combo_activated(const QString &arg1)
+{
+  auto subtype_name = arg1.toStdString();
+  set_corr_combo(subtype_name);
+}
+
+void MainWindow::on_corr_combo_activated(const QString &arg1)
+{
+  auto corr_name = arg1.toStdString();
+  auto correlation = Correlation::search_by_name(corr_name);
+  set_tech_note(correlation->full_desc());
 }
--- pvt-calc/mainwindow.h
+++ pvt-calc/mainwindow.h
@@ -4,7 +4,8 @@
 # include <correlation.H>
 
 #include <QMainWindow>
-
+# include <QLineEdit>
+# include <QComboBox>
 
 namespace Ui {
   class MainWindow;
@@ -14,17 +15,28 @@ class MainWindow : public QMainWindow
 {
   Q_OBJECT
 
+  DynList<string> types;
   DynList<string> subtypes;
   DynList<const Correlation*> correlations;
+  DynList<QLineEdit*> pars_vals;
+  DynList<QComboBox*> pars_units;
 
   void set_substype_combo(const std::string & type_name);
+  void set_corr_combo(const std::string & subtype_name);
+  void set_tech_note(const string & note);
+  void build_corr_entries(const string & corr_name);
 
 public:
   explicit MainWindow(QWidget *parent = 0);
   ~MainWindow();
 
 private slots:
-  void on_corr_type_combo_activated(int index);
+
+  void on_corr_type_combo_activated(const QString &arg1);
+
+  void on_corr_subtype_combo_activated(const QString &arg1);
+
+  void on_corr_combo_activated(const QString &arg1);
 
 private:
   Ui::MainWindow *ui;
