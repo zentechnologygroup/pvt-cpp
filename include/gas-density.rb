ffff3a8638177c42825e3b82188924a1e4ca16f1
Leandro Rabindranath Leon <leandro.r.leon@gmail.com>
2016-10-07 11:50:56 -0400

    Merge con añadiduras de Neylith

--- include/gas-density.rb
+++ include/gas-density.rb
@@ -1,17 +1,8 @@
 begin_correlation("Pg", "GasDensity", "Lb_ft3")
-<<<<<<< HEAD
-add_parameter("yg", "Sgg")
-add_parameter("pb", "psia")
-add_parameter("t", "Rankine")
-add_parameter("p", "psia")
-add_parameter("z", "ZFactor")
-add_author("Standard Equation")
-=======
 add_parameter("yg",  "Sgg", "Aqui Neylith")
 add_parameter("pb",  "psia", "Aqui Neylith")
 add_parameter("t",  "Rankine", "Aqui Neylith")
 add_parameter("p",  "psia", "Aqui Neylith")
 add_parameter("z",  "ZFactor", "Aqui Neylith")
-add_author("Real Gas Law")
->>>>>>> 93f903d2b033c387105a9a25568497e787e57306
+add_author("Standard Equation")
 end_correlation()
