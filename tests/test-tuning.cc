87ca252a400b2a3f6a433163ed6b663a9865c6b0
Leandro Rabindranath Leon <leandro.r.leon@gmail.com>
2016-10-10 17:11:02 -0400

    data empírica

--- tests/test-tuning.cc
+++ tests/test-tuning.cc
@@ -1,9 +1,13 @@
 
 # include <correlations/correlation-stats.H>
-
+# include <metadata/empirical-data.H>
 
 int main()
 {
-  
+  ifstream file("data.csv");
+
+  EmpiricalData e;
+  e.add_samples(file);
+
   return 0;
 }
