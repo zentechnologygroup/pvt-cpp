f882dc280a3c0b8f501056a8a4c955cdd3eaaf07
Ixhel Mejías <ixalejandra@gmail.com>
2016-10-13 11:02:02 -0400

    Error por permisos

--- tests/test-tuning.cc
+++ tests/test-tuning.cc
@@ -1,6 +1,5 @@
 
-# include <correlations/correlation-stats.H>
-# include <metadata/empirical-data.H>
+# include <metadata/correlation-analyzer.H>
 
 int main()
 {
@@ -8,14 +7,43 @@ int main()
 
   EmpiricalData e;
 
-  e.def_const("t", "degF");
-  e.def_const("pb", "psia");
+  e.def_const("t", 189, "degF");
+  e.def_const("api", 26, "api");
+  e.def_const("pb", 3891.98, "psia");
+  e.def_const("rsb", 1110, "SCF_STB");
+  e.def_const("yg", 0.71, "Sgg");
+  e.def_const("tsep", 90, "degF");
+  e.def_const("psep", 60, "psia");
 
   e.add_samples(file);
 
-  e.add_sample({"p = 10", "rs = 10", "bo = 28", "uo = 20"});
-
   cout << e.to_string() << endl;
+
+  CorrelationAnalyser m(e);
+
+  m.target_var = "pb";
+
+  cout << "Data names: ";
+  e.names().for_each([] (const auto & s) { cout << " " << s; });
+  cout << endl
+       << endl;
+
+  m.target_correlations().for_each([] (auto cptr)
+    {
+      cout << cptr->name << ":";
+      cptr->names().for_each([] (const auto & name) { cout << " " << name; });
+      cout << endl;
+    });
+  cout << endl;
+
+  m.target_correlations().filter([&m] (auto cptr)
+				 { return m.can_be_applied(cptr); }).
+    for_each([] (auto p)
+	     {
+	       cout << p->name << ":";
+	       p->names().for_each([] (const auto & s) { cout << " " << s; });
+	       cout << endl;
+	     });
 
   return 0;
 }
