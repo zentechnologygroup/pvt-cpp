14adfc19d003e2e3b63041a7d6231148a9581406
Ixhel Mejías <ixalejandra@gmail.com>
2016-10-27 09:56:18 -0400

    Modificaciones Documentación de sgo y Actualización Base de Datos Referencias

--- include/correlations/oil-interfacial-tension.rb
+++ include/correlations/oil-interfacial-tension.rb
@@ -1,12 +1,14 @@
 # coding: utf-8
 
 # verificada con python!
-begin_correlation("SgoBakerSwerdloff", "OilInterfacialTension", "dynes_cm")
+begin_correlation("SgoBakerSwerdloff", "OilInterfacialTension", "dynes_cm", 0, 35)
 add_parameter("t",  "Fahrenheit", "Temperature")
 add_parameter("api",  "Api" , "API oil gravity")
 add_parameter("p",  "psia", "Pressure")
 add_title("BAKER & SWERDLOFF CORRELATION, CALCULATION OF OIL INTERFACIAL TENSION")
-add_note("The correlation was verified by using secondary references (Beggs, 1987 and Bánzer, 1996) because the original reference is not available. Date: August 30 2016.")
-add_ref("banzer:1996")
+add_internal_note("The correlation was verified by using secondary references (Beggs, 1987 and Bánzer, 1996) because the original reference is not available. Date: August 30 2016.")
 add_author("Baker & Swerdloff")
+add_ref("baker:1956")
+add_ref("beggs:1987")
+add_ref("banzer:1996")
 end_correlation()
