716df84238040ae1e8db6cedbc1fef0c069beccc
Ixhel Mejías <ixalejandra@gmail.com>
2016-10-31 12:42:26 -0400

    Eliminación de parámetros y Modificaciones en documentación Rs

--- include/correlations/solution-gas-oil-relation.rb
+++ include/correlations/solution-gas-oil-relation.rb
@@ -1,7 +1,7 @@
  # coding: utf-8
 
 begin_correlation("RsAlShammasi", "SolutionGasOilRelation", "SCF_STB", 6, 3298.6)
-add_title("AL-SHAMMASI CORRELATION, CALCULATION OF SOLUTION GOR")
+add_title("AL-SHAMMASI CORRELATION, CALCULATION OF SOLUTION GOR (FOR PRESSURES BELOW THE BUBBLE POINT)")
 add_parameter("yg", "Sgg", "Gas specific gravity", 0.51, 3.44)
 add_parameter("p", "psia", "Pressure", "Quantity<Atmosphere>(1)")
 add_parameter("yo", "Sg_do", "Oil specific gravity", "Quantity<Api>(63.7)", "Quantity<Api>(6)")
@@ -14,7 +14,7 @@ end_correlation()
 ################################################################
 
 begin_correlation("RsAlMarhoun", "SolutionGasOilRelation", "SCF_STB", 26, 1602)
-add_title("AL-MARHOUN CORRELATION, CALCULATION OF SOLUTION GOR")
+add_title("AL-MARHOUN CORRELATION, CALCULATION OF SOLUTION GOR (FOR PRESSURES BELOW THE BUBBLE POINT)")
 add_parameter("yg", "Sgg", "Gas specific gravity", 0.752, 1.367)
 add_parameter("p", "psia", "Pressure", 20, 3573)
 add_parameter("yo", "Sg_do", "Oil specific gravity", "Quantity<Api>(44.6)", "Quantity<Api>(19.4)")
@@ -27,7 +27,7 @@ end_correlation()
 ################################################################
 
 begin_correlation("RsDeGhetto", "SolutionGasOilRelation", "SCF_STB", 17.21, 640.25)
-add_title("DE GHETTO, PAONE & VILLA CORRELATION FOR EXTRA-HEAVY (MODIFIED STANDING CORRELATION) AND HEAVY (MODIFIED VASQUEZ & BEGGS CORRELATION) OILS, CALCULATION OF SOLUTION GOR")
+add_title("DE GHETTO, PAONE & VILLA CORRELATION FOR EXTRA-HEAVY (MODIFIED STANDING CORRELATION) AND HEAVY (MODIFIED VAZQUEZ & BEGGS CORRELATION) OILS, CALCULATION OF SOLUTION GOR (FOR PRESSURES BELOW THE BUBBLE POINT)")
 add_parameter("yg", "Sgg", "Gas specific gravity", 0.623, 1.517)
 add_parameter("p", "psia", "Pressure", 1038.49, 7411.54)
 add_parameter("api", "Api", "API oil gravity", 6, 22.3)
@@ -45,7 +45,7 @@ end_correlation()
 ################################################################
 
 begin_correlation("RsDindorukChristman", "SolutionGasOilRelation", "SCF_STB")
-add_title("DINDORUK & CHRISTMAN CORRELATION, CALCULATION OF SOLUTION GOR")
+add_title("DINDORUK & CHRISTMAN CORRELATION, CALCULATION OF SOLUTION GOR (FOR PRESSURES BELOW THE BUBBLE POINT)")
 add_parameter("yg", "Sgg", "Gas specific gravity", 0.6017, 1.027)
 add_parameter("pb", "psia", "Bubble point pressure", 926, 12230)
 add_parameter("p", "psia", "Pressure")
@@ -58,7 +58,7 @@ end_correlation()
 ################################################################
 
 begin_correlation("RsDoklaOsman", "SolutionGasOilRelation", "SCF_STB")
-add_title("DOKLA & OSMAN CORRELATION, CALCULATION OF SOLUTION GOR")
+add_title("DOKLA & OSMAN CORRELATION, CALCULATION OF SOLUTION GOR (FOR PRESSURES BELOW THE BUBBLE POINT)")
 add_parameter("yg", "Sgg", "Gas specific gravity", 0.798, 1.29)
 add_parameter("p", "psia", "Pressure")
 add_parameter("yo", "Sg_do", "Oil specific gravity",  0.8236, 0.886)
@@ -71,7 +71,7 @@ end_correlation()
 ################################################################
 
 begin_correlation("RsGlaso", "SolutionGasOilRelation", "SCF_STB", 90, 2637)
-add_title("GLASO CORRELATION, CALCULATION OF SOLUTION GOR")
+add_title("GLASO CORRELATION, CALCULATION OF SOLUTION GOR (FOR PRESSURES BELOW THE BUBBLE POINT)")
 add_parameter("yg", "Sgg", "Gas specific gravity", 0.65, 1.276)
 add_parameter("p", "psia", "Pressure")
 add_parameter("api", "Api", "API oil gravity", 22.3, 48.1)
@@ -85,8 +85,8 @@ end_correlation()
 
 ################################################################
 
-begin_correlation("RsHanafy", "SolutionGasOilRelation", "SCF_STB", 97, 4272)
-add_title("HANAFY ET AL. CORRELATION, CALCULATION OF SOLUTION GOR")
+begin_correlation("RsHanafy", "SolutionGasOilRelation", "SCF_STB", 7, 4272)
+add_title("HANAFY ET AL. CORRELATION, CALCULATION OF SOLUTION GOR (FOR PRESSURES BELOW THE BUBBLE POINT)")
 add_parameter("p", "psia", "Pressure")
 add_db("Based on experimental PVT data of 324 fluid samples taken from 176 wells located in 75 fields. This data represents 15 productive zones of 123 reservoirs distributed along three different regions of Egypt, including the Gulf of Suez, Western Desert, and Sinai.")
 add_author("Hanafy et al.")
@@ -96,7 +96,7 @@ end_correlation()
 ################################################################
 
 begin_correlation("RsKartoatmodjoSchmidt", "SolutionGasOilRelation", "SCF_STB", 0, 2890)
-add_title("KARTOATMODJO & SCHMIDT CORRELATION, CALCULATION OF SOLUTION GOR")
+add_title("KARTOATMODJO & SCHMIDT CORRELATION, CALCULATION OF SOLUTION GOR (FOR PRESSURES BELOW THE BUBBLE POINT)")
 add_parameter("yg", "Sgg", "Gas specific gravity", 0.4824, 1.668)
 add_parameter("p", "psia", "Pressure")
 add_parameter("api", "Api", "API oil gravity", 14.4, 59)
@@ -112,7 +112,7 @@ end_correlation()
 ################################################################
 
 begin_correlation("RsLasater", "SolutionGasOilRelation", "SCF_STB")
-add_title("LASATER CORRELATION, CALCULATION OF SOLUTION GOR")
+add_title("LASATER CORRELATION, CALCULATION OF SOLUTION GOR (FOR PRESSURES BELOW THE BUBBLE POINT)")
 add_parameter("yg", "Sgg", "Gas specific gravity", 0.574, 1.223)
 add_parameter("p", "psia", "Pressure")
 add_parameter("yo", "Sg_do", "Oil specific gravity", "Quantity<Api>(51.1)", "Quantity<Api>(17.9)")
@@ -127,7 +127,7 @@ end_correlation()
 ################################################################
 
 begin_correlation("RsPetroskyFarshad", "SolutionGasOilRelation", "SCF_STB", 217, 1406)
-add_title("PETROSKY & FARSHAD CORRELATION, CALCULATION OF SOLUTION GOR")
+add_title("PETROSKY & FARSHAD CORRELATION, CALCULATION OF SOLUTION GOR (FOR PRESSURES BELOW THE BUBBLE POINT)")
 add_parameter("yg", "Sgg", "Gas specific gravity", 0.5781, 0.8519)
 add_parameter("p", "psia", "Pressure")
 add_parameter("api", "Api", "API oil gravity", 16.3, 45)
@@ -143,7 +143,7 @@ end_correlation()
 
 ## verificada con python
 begin_correlation("RsStanding", "SolutionGasOilRelation", "SCF_STB", 20, 1425)
-add_title("STANDING CORRELATION, CALCULATION OF SOLUTION GOR")
+add_title("STANDING CORRELATION, CALCULATION OF SOLUTION GOR (FOR PRESSURES BELOW THE BUBBLE POINT)")
 add_parameter("yg", "Sgg", "Gas specific gravity", 0.59, 0.95)
 add_parameter("p", "psia", "Pressure")
 add_parameter("api", "Api", "API oil gravity", 16.5, 63.8)
@@ -159,7 +159,7 @@ end_correlation()
 
 ## verificada con python
 begin_correlation("RsTotalCFP", "SolutionGasOilRelation", "SCF_STB")
-add_title("TOTAL COMPAGNIE FRANÇAISE DES PÉTROLES (CFP) CORRELATION, CALCULATION OF SOLUTION GOR")
+add_title("TOTAL COMPAGNIE FRANÇAISE DES PÉTROLES (CFP) CORRELATION, CALCULATION OF SOLUTION GOR (FOR PRESSURES BELOW THE BUBBLE POINT)")
 add_eq("R_s = \\gamma_g \\cdot \\left[ \\frac{P}{C_1} \\cdot 10 ^ {\\left( C_2 \\cdot \\gamma_{API} - C_3 \\cdot T \\right) } \\right] ^ {C_4}")
 add_parameter("yg", "Sgg", "Gas specific gravity")
 add_parameter("p", "psia", "Pressure")
@@ -175,7 +175,7 @@ end_correlation()
 
 ## verificada con python
 begin_correlation("RsVasquezBeggs", "SolutionGasOilRelation", "SCF_STB", 0, 2199)
-add_title("VAZQUEZ & BEGGS CORRELATION, CALCULATION OF SOLUTION GOR")
+add_title("VAZQUEZ & BEGGS CORRELATION, CALCULATION OF SOLUTION GOR (FOR PRESSURES BELOW THE BUBBLE POINT)")
 add_db("Based on more than 600 laboratory PVT analyses from fields all over the world.")
 add_db("The equation for solution GOR was obtained by regression analysis using 5008 measured data points.")
 add_internal_note("The gas gravity is corrected if it resulted from a first-stage separation at a pressure other than 100 psig (approximately 114.7 psia).")
@@ -195,7 +195,7 @@ end_correlation()
 
 ## verificada con python
 begin_correlation("RsVelarde", "SolutionGasOilRelation", "SCF_STB")
-add_title("VELARDE, BLASINGAME & McCAIN CORRELATION, CALCULATION OF SOLUTION GO")
+add_title("VELARDE, BLASINGAME & McCAIN CORRELATION, CALCULATION OF SOLUTION GOR (FOR PRESSURES BELOW THE BUBBLE POINT)")
 add_db("Based on 195 PVT reports, including 2097 data sets.")
 add_parameter("yg", "Sgg", "Gas specific gravity", 0.561, 1.101)
 add_parameter("pb", "psig", "Bubble point pressure", "Quantity<psia>(106)", "Quantity<psia>(5312)")
@@ -212,7 +212,7 @@ end_correlation()
 
 ## verificada con python
 begin_correlation("RsCegarra", "SolutionGasOilRelation", "SCF_STB", 135, 4290)
-add_title("CEGARRA CORRELATION (MODIFIED STANDING CORRELATION), CALCULATION OF SOLUTION GOR")
+add_title("CEGARRA CORRELATION (MODIFIED STANDING CORRELATION), CALCULATION OF SOLUTION GOR (FOR PRESSURES BELOW THE BUBBLE POINT)")
 add_db("Based on 375 consistent PVT analyses. The data bank was compiled from 8394 fields of eastern Venezuela subdivided into 5192 fields of light oil and 3202 fields of medium oil.")
 add_db("The correlation takes the functional form of Standing.")
 add_parameter("yg", "Sgg", "Gas specific gravity", 0.654, 1.337)
@@ -229,14 +229,11 @@ end_correlation()
 ################################################################
 
 ## verificada con python
-begin_correlation("RsPerezML", "SolutionGasOilRelation", "SCF_STB")
-add_title("PÉREZ, HENY & LAGO CORRELATION (MODIFIED MILLÁN-ARCIA CORRELATION), CALCULATION OF SOLUTION GOR")
-add_parameter("yg", "Sgg", "Gas specific gravity")
+begin_correlation("RsPerezML", "SolutionGasOilRelation", "SCF_STB", 38, 121)
+add_title("PÉREZ, HENY & LAGO CORRELATION (MODIFIED MILLÁN-ARCIA CORRELATION), CALCULATION OF SOLUTION GOR (FOR PRESSURES BELOW THE BUBBLE POINT)")
 add_parameter("pb", "psia", "Bubble point pressure", 405, 1335)
 add_parameter("p", "psia", "Pressure")
-add_parameter("api", "Api", "API oil gravity", 6.4, 12.9)
-add_parameter("t", "Fahrenheit", "Temperature", 112, 300)
-add_parameter("rsb", "SCF_STB", "Solution GOR at Pb", 38, 121)
+add_parameter("rsb", "SCF_STB", "Solution GOR at Pb")
 add_author("Pérez, Heny & Lago")
 add_ref("perez:2001")
 add_internal_note("The correlation could not be verified because the original reference is not available. Date: August 16 2016.")
@@ -247,15 +244,12 @@ end_correlation()
 ################################################################
 
 ## verificada con python
-begin_correlation("RsMillanArcia", "SolutionGasOilRelation", "SCF_STB")
-add_title("MILLÁN-ARCIA CORRELATION, CALCULATION OF SOLUTION GOR")
+begin_correlation("RsMillanArcia", "SolutionGasOilRelation", "SCF_STB", 53, 459)
+add_title("MILLÁN-ARCIA CORRELATION, CALCULATION OF SOLUTION GOR (FOR PRESSURES BELOW THE BUBBLE POINT)")
 add_db("Venezuelan heavy crudes were correlated.")
-add_parameter("yg", "Sgg", "Gas specific gravity")
 add_parameter("pb", "psia", "Bubble point pressure", 222, 3432.7)
 add_parameter("p", "psia", "Pressure")
-add_parameter("api", "Api", "API oil gravity", 9, 20.2)
-add_parameter("t", "Fahrenheit", "Temperature", 87, 195)
-add_parameter("rsb", "SCF_STB", "Solution GOR at Pb", 53, 459)
+add_parameter("rsb", "SCF_STB", "Solution GOR at Pb")
 add_author("Millán-Arcia")
 add_ref("millan:1984")
 add_ref("perez:2001")
@@ -268,7 +262,7 @@ end_correlation()
 
 ## verificada con python
 begin_correlation("RsManucciRosales", "SolutionGasOilRelation", "SCF_STB", 180, 2500)
-add_title("MANUCCI & ROSALES CORRELATION, CALCULATION OF SOLUTION GOR")
+add_title("MANUCCI & ROSALES CORRELATION, CALCULATION OF SOLUTION GOR (FOR PRESSURES BELOW THE BUBBLE POINT)")
 add_db("Crude oils of eastern Venezuela were correlated, by using differential separation data from laboratory PVT analysis.")
 add_parameter("yg", "Sgg", "Gas specific gravity")
 add_parameter("p", "psia", "Pressure")
