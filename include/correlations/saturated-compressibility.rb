3b525714a3a5719e7e55975172ebb8e77668af5d
Ixhel Mejías <ixalejandra@gmail.com>
2016-10-31 10:18:57 -0400

    Modificaciones en documentación Cob

--- include/correlations/saturated-compressibility.rb
+++ include/correlations/saturated-compressibility.rb
@@ -1,7 +1,7 @@
 # verificada con python!
 begin_correlation("CobMcCainEtAl", "SaturatedOilCompressibility", "psia_1",
                   31e-6, 6600e-6)
-add_title("McCAIN, ROLLINS & VILLENA CORRELATION FOR SATURATED OILS (PRESSURES BELOW THE BUBBLE POINT), CALCULATION OF OIL ISOTHERMAL COMPRESSIBILITY")
+add_title("McCAIN, ROLLINS & VILLENA CORRELATION, CALCULATION OF SATURATED OIL ISOTHERMAL COMPRESSIBILITY")
 add_db("Based on 2500 experimental data obtained from different crude oils.")
 add_db("The equation was developed with data from reservoir-fluid studies of 260 oil fields worldwide.")
 add_parameter("api", "Api", "API oil gravity",  18,  52)
