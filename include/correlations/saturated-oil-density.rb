6173c547a1ffa38f0984ed8491b9157eb8eb5fd5
Ixhel Mejías <ixalejandra@gmail.com>
2016-11-02 09:31:19 -0400

    Corrección unidad de bobp y Modificaciones en documentación oil-density (Pob y Poa)

--- include/correlations/saturated-oil-density.rb
+++ include/correlations/saturated-oil-density.rb
@@ -3,12 +3,12 @@
 
 # verificada con python!
 begin_correlation("PobBradley", "SaturatedOilDensity", "Lb_ft3")
-add_title("CALCULATION OF OIL DENSITY")
+add_title("CALCULATION OF SATURATED OIL DENSITY")
 add_parameter("yg",  "Sgg", "Gas specific gravity")
 add_parameter("rs",  "SCF_STB", "Solution GOR")
 add_parameter("bob",  "RB_STB", "Oil formation volume factor")
 add_parameter("yo",  "Sg_do", "Oil specific gravity")
-add_author("Bradley")
+add_author("Standard Equation")
 add_ref("beggs:1987")
 end_correlation()
 
--- include/correlations/undersaturated-oil-density.rb
+++ include/correlations/undersaturated-oil-density.rb
@@ -3,15 +3,15 @@
 
 # verificada con python!
 begin_correlation("PoaBradley", "UndersaturatedOilDensity", "Lb_ft3")
-add_title("CALCULATION OF OIL DENSITY")
+add_title("CALCULATION OF UNDERSATURATED OIL DENSITY")
 add_parameter("yg", "Sgg", "Gas specific gravity")
 add_parameter("rsb", "SCF_STB", "Solution GOR at Pb")
-add_parameter("bobp", "psia", "Oil formation volume factor")
+add_parameter("bobp", "RB_STB", "Oil formation volume factor at Pb")
 add_parameter("yo", "Sgg", "Oil specific gravity")
 add_parameter("p",  "psia", "Pressure")
 add_parameter("pb",  "psia", "Bubble point pressure")
-add_parameter("co",  "psia_1", "Coefficient of isothermal compressibility")
-add_author("Bradley")
+add_parameter("co",  "psia_1", "Oil isothermal compressibility")
+add_author("Standard Equation")
 add_ref("beggs:1987")
 end_correlation()
 
