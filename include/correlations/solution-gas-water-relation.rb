a91294eb29b037df1e82116d6bd714b15f423b5a
Leandro Rabindranath Leon <leandro.r.leon@gmail.com>
2016-11-07 15:07:43 -0400

    Compilado para Ixhel

--- include/correlations/solution-gas-water-relation.rb
+++ include/correlations/solution-gas-water-relation.rb
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-begin_correlation("RswSpiveyMN", "SolutionGasWaterRelation", "Scm3_Scm3")
+begin_correlation("RswSpiveyMN", "SolutionGasWaterRelation", "Sm3_Sm3")
 add_title("SPIVEY, McCAIN & NORTH CORRELATION, CALCULATION OF THE SOLUTION GAS-WATER RATIO")
 add_db("The volumetric properties of oilfield brines were obtained by combining the correlations developed by Spivey et al., for density of methane-free sodium chloride brine and solubility of methane in sodium chloride brines, with published correlations for specific volume and partial molar volume of methane in brine.")
 add_parameter("t", "Celsius", "Temperature",  0,  275) 
@@ -11,6 +11,7 @@ add_ref("spivey:2004")
 add_ref("mcCain:2011")
 add_note("The solution gas-water ratio is obtained by dividing the volume of the gas at standard conditions by the volume of brine at stock-tank conditions.")
 add_internal_note("The correlation was verified by using the original reference and McCain et al. (2011) as a secondary reference. Date: October 17 2016.")
+add_internal_note("The original reference expresses the output unit in Scm3/Scm3")
 end_correlation()
 
 ################################################################
