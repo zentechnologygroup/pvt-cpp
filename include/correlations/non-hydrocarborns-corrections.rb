971542caca154f393553a8dabf6abb3057251c94
Ixhel Mejías <ixalejandra@gmail.com>
2016-11-29 11:42:38 -0400

    Corrección Unidades TscMKayMixingRule y AdjustedTscMWichertAziz

--- include/correlations/non-hydrocarborns-corrections.rb
+++ include/correlations/non-hydrocarborns-corrections.rb
@@ -49,7 +49,7 @@ end_correlation()
 
 ################################################################
 
-begin_correlation("TscMKayMixingRule", "GasPseudocriticalTemperature", "psia")
+begin_correlation("TscMKayMixingRule", "GasPseudocriticalTemperature", "Rankine")
 add_title("KAY'S MIXING RULE, CALCULATION OF PSEUDOCRITICAL TEMPERATURE OF THE WHOLE GAS MIXTURE")
 add_note("The value of the pseudocritical temperature of the gas hydrocarbon portion is adjusted for nonhydrocarbon content on the basis of Kay's mixing rule.")
 add_internal_note("The original reference is not available. The correlation was verified by using secondary references: Whitson & Brulé (2000), Standing (1977) and Bánzer (1996). Date: September 12 2016.")
@@ -57,7 +57,7 @@ add_ref("kay:1936")
 add_ref("whitson:2000")
 add_ref("standing:1977")
 # add_ref("banzer:1996") Secondary reference
-add_parameter("tpchc",  "psia", "Gas pseudocritical temperature of the hydrocarbon portion")
+add_parameter("tpchc",  "Rankine", "Gas pseudocritical temperature of the hydrocarbon portion")
 add_parameter("n2_concentration",  "MoleFraction", "N2 concentration")
 add_parameter("co2_concentration",  "MoleFraction", "CO2 concentration")
 add_parameter("h2s_concentration",  "MoleFraction", "H2S concentration")
@@ -67,7 +67,7 @@ end_correlation()
 ################################################################
 
 begin_correlation("AdjustedTscMWichertAziz", "GasPseudocriticalTemperature",
-                  "psia")
+                  "Rankine")
 add_title("WICHERT & AZIZ CORRELATION, CALCULATION OF PSEUDOCRITICAL TEMPERATURE OF THE WHOLE GAS MIXTURE")
 add_note("The pseudocritical temperature of the mixture is adjusted by using the Wichert & Aziz correlation when the gas contains significant fractions of acid gases, specifically carbon dioxide and hydrogen sulfide.")
 add_ref("wichert:1972")
