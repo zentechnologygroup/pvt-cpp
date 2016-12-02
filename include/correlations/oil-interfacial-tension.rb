a0882a10b640aaded1516d3c8ffc1716801bc745
Ixhel Mejías <ixalejandra@gmail.com>
2016-12-02 10:28:20 -0400

    Se agregaron rangos de T y API para SgoBakerSwerdloff

--- include/correlations/oil-interfacial-tension.rb
+++ include/correlations/oil-interfacial-tension.rb
@@ -2,13 +2,14 @@
 
 # verificada con python!
 begin_correlation("SgoBakerSwerdloff", "OilInterfacialTension", "dynes_cm", 0, 35)
-add_parameter("t",  "Fahrenheit", "Temperature")
-add_parameter("api",  "Api" , "API oil gravity")
+add_parameter("t",  "Fahrenheit", "Temperature", 68, 100)
+add_parameter("api",  "Api" , "API oil gravity", 4, 75)
 add_parameter("p",  "psia", "Pressure")
 add_title("BAKER & SWERDLOFF CORRELATION, CALCULATION OF OIL INTERFACIAL TENSION")
 add_internal_note("The correlation was verified by using secondary references (Beggs, 1987 and Bánzer, 1996) because the original reference is not available. Date: August 30 2016.")
 add_author("Baker & Swerdloff")
 add_ref("baker:1956")
 add_ref("beggs:1987")
+add_ref("petroWiki:2016:4")
 # add_ref("banzer:1996") Secondary reference
 end_correlation()
