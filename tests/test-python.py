602120ed3db1526251f4f3e8661925e3725267c1
Ixhel Mejías <ixalejandra@gmail.com>
2017-01-03 10:22:38 -0400

    Modificaciones test python

--- tests/test-python.py
+++ tests/test-python.py
@@ -66,7 +66,7 @@ def PbDoklaOsmanCorrelation(Yg, Rsb, API, T):
     return PbDoklayOsman
 
 def PbGlasoCorrelation(Yg, Rsb, API, T, n2Concentration, co2Concentration, h2sConcentration):
-    X = (Rsb / Yg) ** 0.816 * T ** 0.172 / API ** 0.989
+    X = (Rsb / Yg) ** 0.816 * (T ** 0.172) / (API ** 0.989)
     PbHC = 10 ** (1.7669 + (1.7447 * log10(X)) - (0.30218 * (log10(X)) ** 2))
     n2Effect = 1 + ((-2.65 * 10 **-4 * API + 5.5 * 10 **-3) * T + (0.0931 * API - 0.8295)) * n2Concentration + ((1.954 * 10 **-11 * API **4.699) * T + (0.027 * API - 2.366)) * n2Concentration **2
     co2Effect = 1 - 693.8 * co2Concentration * T **-1.553
@@ -1101,17 +1101,6 @@ def UoaPerezMLCorrelation(uob, P, Pb):
     uoa = uob * (1 + (3.181 * 1e-4 * (P - Pb)))
     uoaPerezML = uoa
     return uoaPerezML
-    
-def PbGlasoCorrelation(Yg, Rsb, API, T, n2Concentration, co2Concentration, h2sConcentration):
-    X = (Rsb / Yg) ** 0.816 * T ** 0.172 / API ** 0.989
-    PbHC = 10 ** (1.7669 + (1.7447 * log10(X)) - (0.30218 * (log10(X)) ** 2))
-    # Effects of nonhydrocarbons on bubble point pressure
-    n2Effect = 1 + ((-2.65 * 10 **-4 * API + 5.5 * 10 **-3) * T + (0.0931 * API - 0.8295)) * n2Concentration + ((1.954 * 10 **-11 * API **4.699) * T + (0.027 * API - 2.366)) * n2Concentration **2
-    co2Effect = 1 - 693.8 * co2Concentration * T **-1.553
-    h2sEffect = 1 - (0.9035 + 0.0015 * API) * h2sConcentration + 0.019 * (45 - API) *  h2sConcentration **2
-    Pb = PbHC * n2Effect * co2Effect * h2sEffect
-    PbGlaso = Pb
-    return PbGlaso
 
 def SgoBakerSwerdloffCorrelation(T,API,P):
     sgo68 = 39 - 0.2571*API
