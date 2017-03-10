c346093fa22aff1e032d306b971388dbf6d02441
Ixhel Mejías <ixalejandra@gmail.com>
2017-03-10 09:19:45 -0400

    Se ocultaron correlaciones Wet Gas para el grid

--- include/correlations/equivalent-volume.rb
+++ include/correlations/equivalent-volume.rb
@@ -12,6 +12,7 @@ add_note("The equivalent volume is the volume of stock-tank gas plus the volume 
 add_note("This equation is not recommended when the total nonhydrocarbon content of the gas exceeds 25 mol%.")
 add_ref("mcCain:1991")
 add_internal_note("The equation was verified by using the original reference: McCain (1991). Date: March 06 2017.")
+set_hidden_grid()
 end_correlation()
 
 ################################################################
@@ -29,4 +30,5 @@ add_note("The equivalent volume is the volume of stock-tank gas and second-separ
 add_note("This equation is not recommended when the total nonhydrocarbon content of the gas exceeds 25 mol%.")
 add_ref("mcCain:1991")
 add_internal_note("The equation was verified by using the original reference: McCain (1991). Date: March 06 2017.")
+set_hidden_grid()
 end_correlation()
