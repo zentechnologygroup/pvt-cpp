8f55c596d0c96b7762dcd4e44033a6a141504aa7
Leandro Rabindranath Leon <leandro.r.leon@gmail.com>
2017-09-14 13:58:33 -0400

    +

---
+++ tests/gen-grid-test.cc
@@ -0,0 +1,59 @@
+
+# include <tclap/CmdLine.h>
+
+# include <utils.H>
+# include <pvt-grid-compute.H>
+
+using namespace TCLAP;
+
+double
+eq(double k, double c0, double c1, double c2, double c3, double c4,
+   double t, double x)
+{
+  return k*t + c0 + c1+x + c2*pow2(x) + c3*pow3(x) + c4*sin(x);
+}
+
+CmdLine cmd = { "gen-grid-test", ' ', "0.0" };
+
+MultiArg<double> t = { "t", "t", "t", true, "t", cmd };
+ValueArg<double> k = { "", "k", "k", false, 1, "k", cmd };
+ValueArg<double> c0 = { "", "c0", "c0", false, 1, "c0", cmd };
+ValueArg<double> c1 = { "", "c1", "c1", false, 1, "c1", cmd };
+ValueArg<double> c2 = { "", "c2", "c2", false, 1, "c2", cmd };
+ValueArg<double> c3 = { "", "c3", "c3", false, 1, "c3", cmd };
+ValueArg<double> c4 = { "", "c4", "c4", false, 1, "c4", cmd };
+
+DynList<pair<double, DynList<double>>> gen(double first, double last, size_t n)
+{
+  assert(n > 1);
+  assert(first < last);
+  double k = ::k.getValue(), c0 = ::c0.getValue(), c1 = ::c1.getValue(),
+    c2 = ::c2.getValue(), c3 = ::c3.getValue(), c4 = ::c4.getValue();
+  const double step = (last - first) / (n - 1);
+  DynList<pair<double, DynList<double>>> ret;
+  DynList<double> vals;
+  double x = first;
+  for (auto & t : t.getValue())
+    {
+      for (size_t i = 0; i < n; ++i, x += step)
+	vals.append(eq(k, c0, c1, c2, c3, c4, t, x));
+      ret.append(make_pair(t, move(vals)));
+    }
+  return ret;
+}
+
+void gen_grid(const DynList<string> & names,
+	      const DynList<const Unit *> & units,
+	      const DynList<pair<double, DynList<double>>> & vals,
+	      ostream & out)
+{
+  assert(names.size() == units.size());
+  out << join(zip_maps<string>([] (auto t)
+			       { return get<0>(t) + " " + get<1>(t)->name; },
+			       names, units), " ") << endl;
+}
+
+int main(int argc, char *argv[argc])
+{
+  cmd.parse(argc, argv);
+}
