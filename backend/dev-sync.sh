52d66c724fad3c076a13bbb749a1696eae8b95e1
Leandro Rabindranath Leon <leandro.r.leon@gmail.com>
2017-12-06 14:50:12 -0400

    script de co[ia a develop

---
+++ backend/dev-sync.sh
@@ -0,0 +1,6 @@
+scp -P 44350 backend-cplot 138.197.171.24:/var/www/html/develop_pvt/production/php/backend/
+scp -P 44350 backend-ttuner 138.197.171.24:/var/www/html/develop_pvt/production/php/backend/
+scp -P 44350 backend-corr 138.197.171.24:/var/www/html/develop_pvt/production/php/backend/
+scp -P 44350 backend-conversion 138.197.171.24:/var/www/html/develop_pvt/production/php/backend/
+scp -P 44350 backend-ztuner 138.197.171.24:/var/www/html/develop_pvt/production/php/backend/
+scp -P 44350 backend-vector-conversion 138.197.171.24:/var/www/html/develop_pvt/production/php/backend/
