e55f6878ad35db7b066b14fde481f0e3da99bc31
Leandro Rabindranath Leon <Leandro Rabindranath Leon>
2018-02-14 12:32:10 -0400

    +

--- Tests/cplot.cc
+++ Tests/cplot.cc
@@ -15,27 +15,25 @@ struct SimplePlot : public Test
   Cplot cplot;
 };
 
-TEST_F(SimplePlot, Basic)
+TEST_F(SimplePlot, Set_Correlations)
 {
-  ASSERT_THROW(cplot.init_blackoil(), CorrelationNotFound);
-  cplot.set_pb_correlation(&RsAlMarhoun::get_instance());
-  return;
-  ASSERT_THROW(cplot.set_pb_correlation(&RsAlMarhoun::get_instance()),
-	       CorrelationNotFound);
-  cplot.set_pb_correlation(&PbAlMarhoun::get_instance());
-  
-  ASSERT_THROW(cplot.init_blackoil(), CorrelationNotFound);
-  cplot.set_rs_correlation(&RsAlMarhoun::get_instance());
-  ASSERT_THROW(cplot.init_blackoil(), CorrelationNotFound);
+  // This would be the general structure of this test
+  ASSERT_THROW(cplot.set_pb(RsAlMarhoun::get_instance()),
+	       InvalidTargetType);
+  ASSERT_NO_THROW(cplot.set_pb(PbAlMarhoun::get_instance()));
 
-  cplot.set_bob_correlation(&BobAlmarhoun::get_instance());
-  ASSERT_THROW(cplot.init_blackoil(), CorrelationNotFound);
-  cplot.set_coa_correlation(&CoaDeGhetto::get_instance());
+  ASSERT_THROW(cplot.set_pb(&RsAlMarhoun::get_instance()),
+	       InvalidTargetType);
+  ASSERT_NO_THROW(cplot.set_pb(&PbAlShammasi::get_instance()));
 
-  ASSERT_THROW(cplot.init_blackoil(), CorrelationNotFound);
-  cplot.set_bob_correlation(&BoaMcCain::get_instance());
-  ASSERT_THROW(cplot.init_blackoil(), CorrelationNotFound);
+  ASSERT_NO_THROW(cplot.set_pb(PbAlMarhoun::get_instance(), 2, 2));
+  ASSERT_EQ(cplot.c_pb, 2);
+  ASSERT_EQ(cplot.m_pb, 2);
 
-  cplot.set_bob_correlation(&BoaMcCain::get_instance());
-  ASSERT_THROW(cplot.init_blackoil(), CorrelationNotFound);
+  // The remaining tests would be similar but with others correlations
+}
+
+TEST_F(SimplePlot, Verify_static_method)
+{
+  ASSERT_THROW(cplot.verify_correlation(cplot.pb_corr, "rs"), InvalidTargetType);
 }
