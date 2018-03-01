262500d90b2875b279f86b84857561b856ac9975
Leandro Rabindranath Leon <leandro.r.leon@gmail.com>
2018-03-01 17:27:31 -0400

    +

--- Tests/cplot.cc
+++ Tests/cplot.cc
@@ -38,18 +38,18 @@ TEST(Grid, GridClass)
   ASSERT_TRUE(eq(grid.col("rs"), {90, 140, 340, 500}));
 }
 
-struct SimplePlot : public Test
+struct TestPlot : public Test
 {
   BlackoilGrid cplot;
 
-  SimplePlot()
+  TestPlot()
     : cplot(Fahrenheit::get_instance(), psia::get_instance(), true)
   {
     set_ttuner_units();
   }
 };
 
-struct BlackoilPlot : public SimplePlot
+struct BlackoilPlot : public TestPlot
 {
   BlackoilPlot()
   {
@@ -83,7 +83,42 @@ struct BlackoilPlot : public SimplePlot
   }
 };
 
-TEST_F(SimplePlot, Set_Optional_Correlations)
+struct SimplePlot : public Test
+{
+  SimpleGrid cplot;
+  
+  SimplePlot() : cplot(Fahrenheit::get_instance(), psia::get_instance(), true)
+  {
+    cplot.set_boa(BoaMillanArcia::get_instance(),
+		  -0.260005454479017, 1.23760105495816);
+    cplot.set_bob(BobTotalCFP::get_instance(),
+		  -0.743320864010538, 1.73939210267341);
+    cplot.set_cob(CobMcCainEtAl::get_instance());
+    cplot.set_coa(CoaPerezML::get_instance(),
+		  8.6296577461858e-06, -0.477971383291644);
+    cplot.set_pb(PbSalazar::get_instance(),
+		 58.9216674773746, 0.832262895375856);
+    cplot.set_rs(RsSalazar::get_instance(),
+		 -2.07970430374413, 1.17945155501256);
+    cplot.set_uoa(UoaDeGhettoEtAl::get_instance(),
+		  32.567065703422, 1.0);
+    cplot.set_uob(UobPerezML::get_instance(),
+		  -138.470633573862, 1.18174982673783);
+    cplot.set_uod(UodPerezML::get_instance(), -4124.34486516297, 1.0);
+    cplot.set_zfactor(ZfactorDranchukAK::get_instance());
+    cplot.set_api(8.3, Api::get_instance());
+    cplot.set_co2(.86, MolePercent::get_instance());
+    cplot.set_h2s(0, MolePercent::get_instance());
+    cplot.set_n2(.19, MolePercent::get_instance());
+    cplot.set_nacl(0, Molality_NaCl::get_instance());
+    cplot.set_psep(100, psia::get_instance());
+    cplot.set_rsb(79.5, SCF_STB::get_instance());
+    cplot.set_tsep(100, Fahrenheit::get_instance());
+    cplot.set_yg(.608, Sgg::get_instance());
+  }
+};
+
+TEST_F(TestPlot, Set_Optional_Correlations)
 {
   // This would be the general structure of this test
   ASSERT_THROW(cplot.set_cob(RsAbovePb::get_instance()), InvalidTargetType);
@@ -91,7 +126,7 @@ TEST_F(SimplePlot, Set_Optional_Correlations)
   // The remaining tests would be similar but with others correlations
 }
 
-TEST_F(SimplePlot, Set_Mandatory_Correlations)
+TEST_F(TestPlot, Set_Mandatory_Correlations)
 {
   // This would be the general structure of this test
   ASSERT_THROW(cplot.set_pb(RsAlMarhoun::get_instance()),
@@ -109,7 +144,7 @@ TEST_F(SimplePlot, Set_Mandatory_Correlations)
   // The remaining tests would be similar but with others correlations
 }
 
-TEST_F(SimplePlot, Verify_static_method)
+TEST_F(TestPlot, Verify_static_method)
 {
   ASSERT_NO_THROW(cplot.verify_correlation(&RsAlMarhoun::get_instance(), "rs"));
   ASSERT_THROW(cplot.verify_correlation(&RsAlMarhoun::get_instance(), "pb"),
@@ -118,32 +153,32 @@ TEST_F(SimplePlot, Verify_static_method)
   ASSERT_THROW(cplot.verify_correlation(cplot.pb_corr, "rs"), InvalidTargetType);
 }
 
-TEST_F(SimplePlot, blackoil_correlations_are_set)
+TEST_F(TestPlot, blackoil_correlations_are_set)
 {
-  ASSERT_THROW(cplot.blackoil_correlations_are_set(), CorrelationNotFound);
+  ASSERT_THROW(cplot.correlations_are_set(), CorrelationNotFound);
   cplot.set_pb(PbAlMarhoun::get_instance());
-  ASSERT_THROW(cplot.blackoil_correlations_are_set(), CorrelationNotFound);
+  ASSERT_THROW(cplot.correlations_are_set(), CorrelationNotFound);
   cplot.set_rs(RsAlMarhoun::get_instance());
-  ASSERT_THROW(cplot.blackoil_correlations_are_set(), CorrelationNotFound);
+  ASSERT_THROW(cplot.correlations_are_set(), CorrelationNotFound);
   cplot.set_bob(BobAlShammasi::get_instance());
-  ASSERT_THROW(cplot.blackoil_correlations_are_set(), CorrelationNotFound);
+  ASSERT_THROW(cplot.correlations_are_set(), CorrelationNotFound);
   cplot.set_cob(CobMcCainEtAl::get_instance());
-  ASSERT_THROW(cplot.blackoil_correlations_are_set(), CorrelationNotFound);
+  ASSERT_THROW(cplot.correlations_are_set(), CorrelationNotFound);
   cplot.set_coa(CoaDeGhetto::get_instance());
-  ASSERT_THROW(cplot.blackoil_correlations_are_set(), CorrelationNotFound);
+  ASSERT_THROW(cplot.correlations_are_set(), CorrelationNotFound);
   cplot.set_boa(BoaMcCain::get_instance());
-  ASSERT_THROW(cplot.blackoil_correlations_are_set(), CorrelationNotFound);
+  ASSERT_THROW(cplot.correlations_are_set(), CorrelationNotFound);
   cplot.set_uod(UodBeal::get_instance());
-  ASSERT_THROW(cplot.blackoil_correlations_are_set(), CorrelationNotFound);
+  ASSERT_THROW(cplot.correlations_are_set(), CorrelationNotFound);
   cplot.set_uob(UobBeggsRobinson::get_instance());
-  ASSERT_THROW(cplot.blackoil_correlations_are_set(), CorrelationNotFound);
+  ASSERT_THROW(cplot.correlations_are_set(), CorrelationNotFound);
   cplot.set_uoa(UoaAbedini::get_instance());
-  ASSERT_THROW(cplot.blackoil_correlations_are_set(), CorrelationNotFound);
+  ASSERT_THROW(cplot.correlations_are_set(), CorrelationNotFound);
   cplot.set_zfactor(ZfactorBrillBeggs::get_instance());
-  ASSERT_NO_THROW(cplot.blackoil_correlations_are_set());
+  ASSERT_NO_THROW(cplot.correlations_are_set());
 }
 
-TEST_F(SimplePlot, Set_parameters)
+TEST_F(TestPlot, Set_parameters)
 {
   ASSERT_THROW(cplot.test_api(), ValueNotFound);
   ASSERT_THROW(cplot.test_rsb(), ValueNotFound);
@@ -190,4 +225,15 @@ TEST_F(BlackoilPlot, generate)
 {
   BlackoilGrid::Grid grid =
     cplot.generate_grid({100, 200, 300}, {100, 300, 700});
+
+  //  cout << grid << endl;
 }
+
+TEST_F(SimplePlot, generate)
+{
+  SimpleGrid::Grid grid =
+    cplot.generate_grid({100, 200, 300}, {100, 300, 700});
+
+  cout << grid << endl;
+}
+
